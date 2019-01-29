55a3041cf60805004d52894a2c6e04482cc99b44
Yash Rajgor <yash592@gmail.com>
2019-01-29 01:07:14 -0800

    basicsapi2

--- controllers/newsSearchController.js
+++ controllers/newsSearchController.js
@@ -6,7 +6,8 @@ module.exports = {
     let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=513740817e1e424cb4406d9e434de94f'
     axios.get(url)
       .then(response => {
-        console.log(response.data);
+        console.log("response" , response);
+        res.json(response.data.articles)
       })
   }
 }
