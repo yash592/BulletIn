4cd07f7d787da6a2fcb7a74d90265fba18a196de
Yash Rajgor <yash592@gmail.com>
2019-01-31 00:40:15 -0800

    loadingAnimation

---
+++ client/src/components/loading/loading.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+const Loading = () => <div style={{flexDirection: 'column',
+justifyContent: 'center',
+alignItems: 'center',
+height: '100%'}}><img src = "https://i.imgur.com/fgeZcwX.gif"/></div>
+
+
+
+export default Loading;
