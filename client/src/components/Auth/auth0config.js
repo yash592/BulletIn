4686270e09ae16bb9252605335a822500fc4b495
Yash Rajgor <yash592@gmail.com>
2019-02-05 20:13:16 -0800

    Update auth0config.js

--- client/src/components/Auth/auth0config.js
+++ client/src/components/Auth/auth0config.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'bulletinio.auth0.com',
   clientId: 'nDd3vyX4BTMItXliv4-Cvy7VPFCxgaw1',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://bulletinio.herokuapp.com/callback'
+//   'http://localhost:3000/callback'
 }
