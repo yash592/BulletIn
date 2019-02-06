e5cc3b68b502d6e876b39b1dc73f5be159164356
Yash Rajgor <yash592@gmail.com>
2019-02-05 16:47:25 -0800

    github login

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,8 @@ const userSaveController = require('../../controllers/userSaveController');
 
 router
   .route("/")
-  .post(userSaveController.create);
+  .get(userSaveController.getSavedUsers)
+  .post(userSaveController.create)
+  .post(userSaveController.update)
 
 module.exports = router;
