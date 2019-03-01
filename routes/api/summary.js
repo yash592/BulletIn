9063d52bdc5305aae34138fc26e4df3d3446c4a4
Yash Rajgor <yash592@gmail.com>
2019-02-28 21:04:46 -0800

    summary

--- routes/api/summary.js
+++ routes/api/summary.js
@@ -1,1 +1,10 @@
 // api route for summary
+
+const router = require('express').Router();
+const newsSummaryController = require('../../controllers/newsSummaryController');
+
+router
+  .route("/")
+  .get(newsSummaryController.summarize);
+
+module.exports = router;
