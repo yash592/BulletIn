ed7e0dde31776bcbe8254659058f57cad9224c72
Yash Rajgor <yash592@gmail.com>
2019-03-05 10:59:32 -0800

    deployv21

--- routes/api/summary.js
+++ routes/api/summary.js
@@ -1,12 +1,12 @@
 // api route for summary
 
-const router = require('express').Router();
-const newsSummaryController = require('../../controllers/newsSummaryController');
-
-// Matches with "/api/summary"
-
-router
-  .route("/")
-  .get(newsSummaryController.summarize);
-
-module.exports = router;
+// const router = require('express').Router();
+// const newsSummaryController = require('../../controllers/newsSummaryController');
+//
+// // Matches with "/api/summary"
+//
+// router
+//   .route("/")
+//   .get(newsSummaryController.summarize);
+//
+// module.exports = router;
