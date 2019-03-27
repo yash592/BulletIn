dd0340f0e934eea19f14b4d832a1b9cfd5da2329
Yash Rajgor <yash592@gmail.com>
2019-03-26 23:33:31 -0700

    searchV2

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { Input } from 'semantic-ui-react'
 import { Form, Button } from 'react-bootstrap'
 
-const InputExampleIcon = () =>
-<div  style={{display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Control type="Search" placeholder="Enter a search term" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-</div>
+const InputExampleIcon = props => (
+  <div  style={{display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+  <form>
+    <label>
+      <input type="text" placeholder="Enter a search term"  value={props.value} onChange={props.handleChange}/>
+    </label>
+    <Input type="submit" value="Search"  onClick={props.handleSearch}/>
+  </form>
+  </div>
+)
+
 
 export default InputExampleIcon
