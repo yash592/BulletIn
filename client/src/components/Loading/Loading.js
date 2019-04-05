b0d7fc2877b0df919746cd1f8b6a0447344d0d1e
Yash Rajgor <yash592@gmail.com>
2019-04-04 19:29:01 -0700

    Navbar fixes in progress

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 const Loading = () =>
-  <div style={{dislay: 'flex',alignItems: 'center', float: 'center'}}>
+  <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh'}}>
     <img src = "https://i.imgur.com/fgeZcwX.gif" style={{width:50, height: 50}}/>
   </div>
 
